ad19fae06ffacfb9a6c288eac19f44e9f5970822
Fiona.Dick <fiona.dick@uib.no>
2019-02-12 14:22:38 +0100

    changed Tuning to be a class metho varImpTuning sets the TuningObj attributes after Tuning added line in main to save params and oob to file as well as trying to save scala obj (tuning extends serializable)

--- src/Tuning.scala
+++ src/Tuning.scala
@@ -1,27 +1,72 @@
 import au.csiro.variantspark.algo.RandomForestParams
 import au.csiro.variantspark.api.{ImportanceAnalysis, VSContext}
 import au.csiro.variantspark.input.{FeatureSource, LabelSource}
+import ca.innovativemedicine.vcf.Type
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
-object Tuning {
+@SerialVersionUID(100L)
+class Tuning(sparkSession: SparkSession) extends Serializable {
+  // vars
+  private var _bestParam: Tuple2[Int, Double] = (0, 0)
+  private var _minOOB: Double = 1
+  private var _TuningResultDf: DataFrame = sparkSession.emptyDataFrame
+  private var _top10s: Array[String] = Array("")
 
-  def varImpTuning(vsContext: VSContext, featureSource: FeatureSource, labelSource: LabelSource, nTreeParams: Array[Int], mtryFracParams: Array[Double]): Tuple2[Int, Double] = {
+
+  val schema = new StructType()
+  schema.add(StructField("oobErr", DoubleType))
+  schema.add(StructField("nTree", IntegerType))
+  schema.add(StructField("mTry", DoubleType))
+
+
+  // Getter
+  def bestParam: Tuple2[Int, Double] = _bestParam
+
+  def minOOB: Double = _minOOB
+
+  def TuningResultDf: DataFrame = _TuningResultDf
+
+  def top10s: Array[String] = _top10s
+
+  // Setter
+  def bestParam_=(value: Tuple2[Int, Double]): Unit = _bestParam = value
+
+  def minOOB_=(value: Double): Unit = _minOOB = value
+
+  def TuningResultDf_=(value: DataFrame): Unit = _TuningResultDf = value
+
+  def top10s_=(value: Array[String]): Unit = _top10s = value
+
+  //methods
+
+  def varImpTuning(vsContext: VSContext, featureSource: FeatureSource, labelSource: LabelSource, nTreeParams: Array[Int], mtryFracParams: Array[Double], spark: SparkSession): Unit = {
+
     val variantSparkModels = (for (nTree <- nTreeParams; mtryFrac <- mtryFracParams) yield {
 
       val importanceAnalysis = ImportanceAnalysis(featureSource, labelSource, nTrees = nTree, rfParams = RandomForestParams(oob = true, nTryFraction = mtryFrac))(vsContext = vsContext)
       val oobErr: Double = importanceAnalysis.rfModel.oobError
-      (oobErr, Tuple2(nTree, mtryFrac))
+      print(s"oobErr: $oobErr - mTryFrac: $mtryFrac - nTree: $nTree")
+      val top10 = importanceAnalysis.importantVariables(10).map { x => x._1 }.mkString(";")
+      (oobErr, Tuple2(Tuple2(nTree, mtryFrac), top10))
     }).toMap
-    val minErr = variantSparkModels.keys.min
-    val bestParams = variantSparkModels.get(minErr)
-    bestParams match {
-       case Some(i) => {
-         println(s"Lowest oobErr was: $minErr with parameters: $i")
-         return (i)
-       }
-       case None => {
-         println("Didnt find oobErr (key) in Tuning Map")
-         return (null)
-       }
+    _minOOB = variantSparkModels.keys.min
+    val rdd = sparkSession.sparkContext.parallelize(variantSparkModels.map { x => Tuple2(x._1, x._2._1) }.toSeq)
+      .map { x => Row(x._1, x._2._1, x._2._1) }
+    _TuningResultDf = sparkSession.createDataFrame(rdd, this.schema)
+    _top10s = variantSparkModels.map { x => x._2._2 }.toArray
+
+    val ParamsOpt = variantSparkModels.get(_minOOB)
+
+    ParamsOpt match {
+      case Some(i) => {
+        println(s"Lowest oobErr was: $_minOOB with parameters: ${i._1}")
+        _bestParam = i._1
+
       }
+      case None => {
+        println("Didnt find oobErr (key) in Tuning Map")
+      }
+    }
   }
 }
--- src/VCF_BinaryClassifierPipe.scala
+++ src/VCF_BinaryClassifierPipe.scala
@@ -19,6 +19,8 @@ import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.mllib.evaluation
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types.{DoubleType, IntegerType, StructField, StructType}
+import java.io
+import java.io.{FileOutputStream, ObjectOutputStream}
 
 
 
@@ -34,13 +36,16 @@ object VCF_BinaryClassifierPipe {
       .enableHiveSupport()
       .getOrCreate()
 
-    implicit val vsContext = VSContext(spark)
+    implicit val vsContext : VSContext = VSContext(spark)
 
+    val timeStamp : Long = System.currentTimeMillis / 1000
 
-    val nTreeParams = Array(10,100,1000,10000)
+    /*val nTreeParams = Array(10,100,1000,10000)
+    val mtryFracParams = Array(0.1,0.2,0.25,0.3,0.35,0.4)
+   */
+    val nTreeParams = Array(1,2)
     val NtopParams = Array(100,500,1000,2000)
-
-    val mtryFracParams = Array(0.1,0.15,0.2,0.25,0.3,0.35,0.4)
+    val mtryFracParams = Array(0.1,0.05)
 
     val featureSource = vsContext.featureSource("/data/content/vcf_classification/data_used/trainSplit.vcf")
 
@@ -49,9 +54,25 @@ object VCF_BinaryClassifierPipe {
     val featureSourceTest=vsContext.featureSource("/data/content/vcf_classification/data_used/testSplit.vcf")
     val labelSourceTest = vsContext.labelSource("/data/content/vcf_classification/data_used/labels_test.txt", "label")
 
-    val paramsVarImp = Tuning.varImpTuning(vsContext,featureSource,labelSource,nTreeParams,mtryFracParams)
 
-    val importanceAnalysis = ImportanceAnalysis(featureSource, labelSource, nTrees = paramsVarImp._1, rfParams = RandomForestParams(oob = true, nTryFraction = paramsVarImp._2))
+    val TuningObj = new Tuning(spark)
+
+    TuningObj.varImpTuning(vsContext,featureSource,labelSource,nTreeParams,mtryFracParams,spark)
+    val filename = "/data/content/vcf_classification/results/tuningVI/" + timeStamp + ".txt"
+    TuningObj.TuningResultDf.write.csv(filename)
+
+ // (2) write the instance out to a file
+    val oos = new ObjectOutputStream(new FileOutputStream(filename + ".ScalaObj"))
+        oos.writeObject(TuningObj)
+        oos.close
+// (3) read the object back in
+  //  val ois = new ObjectInputStream(new FileInputStream("/tmp/nflx"))
+   // val stock = ois.readObject.asInstanceOf[Stock]
+    //ois.close
+
+
+   /*
+    val importanceAnalysis = ImportanceAnalysis(featureSource, labelSource, nTrees = TuningObj.bestParam._1, rfParams = RandomForestParams(oob = true, nTryFraction = TuningObj.bestParam._2))
 
     val NtopResults = NtopParams.map { Ntop =>
 
@@ -143,6 +164,7 @@ object VCF_BinaryClassifierPipe {
 
 
     } // nTop map
+    */
 
   }//main
 
